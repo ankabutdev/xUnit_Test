232ffd3dbac4557c3432ff59b9c13c60ab55b329
ankabutdev <Ankabut571@gmail.com>
2023-12-15 21:51:52 +0500

    improve

---
+++ xUnit_Demo/MathExtensions/MathExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace xUnit_Demo.MathExtensions;
+
+public static class MathExtension
+{
+    public static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+
+        for (int i = 2; i < number; i++)
+        {
+            if (i % 2 == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
