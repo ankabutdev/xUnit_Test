86e76389affa10ce03e3d3af0ec4b51b71014e46
ankabutdev <Ankabut571@gmail.com>
2023-12-15 21:52:24 +0500

    improve

---
+++ xUnit_Demo/Services/Users/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using xUnit_Demo.Models;
+
+namespace xUnit_Demo.Services.Users;
+
+public interface IUserService
+{
+    public ValueTask<IEnumerable<User>> GetAllUsers();
+
+    public ValueTask<User> GetByIdAsync(int userId);
+
+    public ValueTask<bool> CreateAsync(User user);
+
+    public ValueTask<bool> UpdateAsync(int Id, User user);
+
+    public ValueTask<bool> DeleteAsync(int UserId);
+
+}
