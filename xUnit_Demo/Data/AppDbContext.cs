ae6b8ca86f8e0dae1e5e6b4cb0e076d0e9f132c5
ankabutdev <Ankabut571@gmail.com>
2023-12-15 21:51:48 +0500

    improve

---
+++ xUnit_Demo/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using xUnit_Demo.Models;
+
+namespace xUnit_Demo.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+}
